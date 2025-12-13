12c03c1ee3947ff90ac588720f9703508c337d5e
1F8C010F837214B9E4BFB1A258829508 <165718547+isaacward1@users.noreply.github.com>
2025-12-12 21:55:43 -0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -10,35 +10,40 @@ $nat_mac = "xx:xx:xx:xx:xx:xx" # MAC address of your NAT interface
 $nat_ip = "192.168.100.2" # IP you want VM to have on the NAT network
 $nat_gateway = "192.168.100.1" # gateway IP of NAT interface 
 $nat_subnet = 30 # subnet prefix length for NAT adapter
+
+$http_server_port = 8888 # change to desired server port
 # ========================================================================= #
 
 # configuring host-only adapter
+write-host "configuring host-only adapter..."
 $hostonly_mac=$hostonly_mac.ToUpper().Replace(":", "-")
 $hostonly_adpt_idx = (Get-NetAdapter | select ifIndex, MacAddress | Where-Object {$_.MacAddress -eq $hostonly_mac}).ifIndex
 New-NetIPAddress -InterfaceIndex $hostonly_adpt_idx -IPAddress $hostonly_ip -PrefixLength $hostonly_subnet -DefaultGateway $hostonly_gateway
 Set-DnsClientServerAddress -InterfaceIndex $hostonly_adpt_idx -ServerAddresses $hostonly_gateway
 
 # configuring NAT adapter
+write-host "configuring NAT adapter..."
 $nat_mac=$nat_mac.ToUpper().Replace(":", "-")
 $nat_adpt_idx = (Get-NetAdapter | select ifIndex, MacAddress | Where-Object {$_.MacAddress -eq $nat_mac}).ifIndex
 New-NetIPAddress -InterfaceIndex $nat_adpt_idx -IPAddress $nat_ip -PrefixLength $nat_subnet -DefaultGateway $nat_gateway
 Set-DnsClientServerAddress -InterfaceIndex $nat_adpt_idx -ServerAddresses $nat_gateway
 
-# python http server config
+# allow host machine to access python server
+write-host "Creating firewall rule to allow host to access python server..."
 $path_to_python = (Get-Command python).source
-$http_server_port = 8888 # change to desired server port
-
-# allow guest to access python server
 New-NetFirewallRule -DisplayName "allow_in_host_to_vm_pyhttp" -Enabled True -Action Allow -Direction Inbound -LocalAddress $hostonly_ip -LocalPort $http_server_port -Protocol TCP -RemoteAddress $hostonly_gateway -Profile Any -Program $path_to_python
 
 # disabling redundant rule created by fakenet
+write-host "Disabling stupid fw rule..."
 Set-NetFirewallRule -DisplayName "inbound from internet = block" -Enabled False
 
 # installing Chocolatey
+write-host "installing Chocolatey..."
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # installing tools via Chocolatey
+write-host "installing tools..."
 $tools = @(
   'pestudio', 'imhex', 'x64dbg.portable', 'ghidra', 
   'cyberchef', 'floss', 'die', 'capa', 'wireshark', 
@@ -49,5 +54,4 @@ foreach ($tool in $tools) {
   choco install $tool -y
 }
 
-
-
+write-host "Done!"
